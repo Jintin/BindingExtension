1c0e8d93ba3de5b296013066f3331645bb88307f
Jintin <jintin.lin1018@gmail.com>
2020-12-19 09:13:42 +0800

    rename

--- lib/src/main/java/com/jintin/bindingextension/BindingActivity.kt
+++ lib/src/main/java/com/jintin/bindingextension/BindingActivity.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
-open class BindingActivity<V : ViewBinding>(private val inflater: (LayoutInflater) -> V) :
-    AppCompatActivity() {
+open class BindingActivity<V : ViewBinding>(
+    private val bindingProvider: (LayoutInflater) -> V
+) : AppCompatActivity() {
 
     lateinit var binding: V
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = inflater.invoke(layoutInflater)
+        binding = bindingProvider.invoke(layoutInflater)
         setContentView(binding.root)
     }
 }
